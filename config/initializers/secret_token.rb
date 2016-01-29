676c0f8af9490ca90ee35392f0134910b2ed0bf1
Marcin Pasternak <marcin.pasternak@copper.io>
2016-01-29 15:10:22 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TSCloudRailsSampleApp::Application.config.secret_token = '9e8358b8436a51cf34a54171f87b1b8f1798f17a4ed76ca12e21db7add82e116ed8c4978d4487d3c11b4f67e910f57ba24690c009bce01355497087d9a79d825'
